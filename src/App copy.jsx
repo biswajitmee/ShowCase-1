47c29b4c19b1de94bc60800bd7e2a0c4c4d01d25
biswajitmee <biswajit.mee@gmail.com>
2024-04-25 20:01:00 +0530

    horizantal scroll will start

--- src/App copy.jsx
+++ src/App copy.jsx
@@ -11,32 +11,31 @@ function App() {
   const smoother = useRef();
   const wrapperPinRef = useRef(null);
 
- 
-  
-
   useGSAP(() => {
     smoother.current = ScrollSmoother.create({
-      smooth: 1,
+      smooth: 2,
       effects: true,
-      smoothTouch: 0.5,
+      smoothTouch: 2.5,
     });
 
  
-    
-    let scrollBoxAry = gsap.utils.toArray(".scrollBox");
+    let scrollBox = gsap.utils.toArray(".scrollBox");
 
     let containerWidth = document.querySelector('.HorizetalScroll').offsetWidth;
     
-    let scrollTween = gsap.to(scrollBoxAry, {
-        xPercent: -100 * (scrollBoxAry.length - 1),
-        ease: "none", // <-- IMPORTANT!
-        scrollTrigger: {
-          trigger: "#wraperPinSection",
-          pin: true,
-          scrub: true,
-           end: "+=" + containerWidth 
-        }
-      });
+    let scrollTween = gsap.to(scrollBox, {
+      xPercent: -100 * (scrollBox.length - 1),
+      duration:50,
+      ease: "none", // <-- IMPORTANT!
+  });
+  
+  ScrollTrigger.create({
+      trigger: "#wraperPinSection",
+      pin: true,
+      scrub: 0.0001, // Adjust the scrub value for scroll sensitivity
+      end: "+=" + containerWidth,
+      animation: scrollTween
+  });
  
  
   }, { scope: main });
@@ -59,7 +58,7 @@ function App() {
         {/* in active pin horizontal scroll the div - x axis */}
             <div className="HorizetalScroll"   >
               <div className="scrollBox subDiv">
-                We're revolutionizing section 1
+                We're revolutionizing section 3
               </div>
               <div className="scrollBox subDiv">
                 digital creativity,
@@ -71,19 +70,19 @@ function App() {
                 and low-code synergy.
               </div>
               <div className="scrollBox subDiv">
+                We're revolutionizing
+              </div>
+              {/* <div className="scrollBox subDiv">
                 digital creativity,
               </div>
               <div className="scrollBox subDiv">
                 empowering brands with high-design
-              </div>
-              <div className="scrollBox subDiv">
-                and low-code synergy. last
-              </div>
+              </div> */}
             </div>
           </section>
           <section className='bg-lime-400 section3'>
             <h1 className="text-3xl font-bold underline">
-              Hello world! section 4
+              Hello world! section last
             </h1>
           </section>
         </div>
